7af3ba346441f4959dc51bd24ec2d2e2a7b05eea
Christian_Fredericksen <christian.fredericksen@yahoo.com>
2020-04-02 19:19:40 -0700

    edit users_controller/authenticate user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,40 @@
 class UsersController < ApplicationController
-    # get "/users/signup" do
-    #     erb :"/users/new"
-    # end
-
-    get "/users/signup" do
-        erb :"/users/new"
+    #CREATE    
+    get "/signup" do
+        erb :'/users/signup'
     end
 
-    post "/users/new" do
-        @user = User.create(params[:user])       
-        @user.save
-        session[:user_id] = @user.id
-
-        redirect '/users/show'   
+    post "/signup" do
+        
+        if params[:name] == ""  || params[:email] == ""  || params[:password] == ""
+            redirect "/failure"
+        else
+          @user = User.create(:name => params[:name], :email => params[:email], :password => params[:password])          
+          session[:user_id] = @user.id
+          redirect "/users/#{@user.id}"
+        end   
     end
 
-    
+    get '/failure' do
+        erb :'/users/signup'
+    end
 
-    get '/users/show' do 
+    #READ
+    get '/users' do 
+        @users = User.all
+        erb :'/index' 
+    end
+
+    get '/users/:id' do 
         @user = User.find(session[:user_id])
         erb :'/users/show'
+    end 
+    
+    #UPDATE
+    get '/users/:id/edit' do
+        @user = User.find(session[:user_id])
+        erb :'/users/edit'
     end
+    
 
-    
-    
-    get '/users/:id' do 
-        @user = User.find(params[:id])
-        erb :'/users/show'
-    end
 end
