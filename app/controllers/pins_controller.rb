b138632211ad8b007ee11ec94c45c797be72eee5
Christian_Fredericksen <christian.fredericksen@yahoo.com>
2020-04-05 11:01:15 -0700

    cleaned up /users/index.erb

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,12 +1,8 @@
 class PinsController < ApplicationController
 
-   get '/collection' do
-    @user = @user.name
-    erb :/pins/show
-   end 
-
-   get '/collection/:id' do
-    "Look at all those pins!"
-   end 
-    
-end
+    get '/collection/:id' do
+        #@user = User.find(params[:id])
+        erb :"/pins/show"
+    end 
+ 
+ end
